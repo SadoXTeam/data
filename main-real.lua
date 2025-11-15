d59bf31b7574ea27d5e80744e6000411bfb6caa4
SadoXTeam <anhquanutroblox@gmail.com>
2025-11-15 15:52:19 +0700

    Create main-real.lua

---
+++ main-real.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SadoXTeam/data/refs/heads/main/a"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SadoXTeam/data/refs/heads/main/desyncdata"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SadoXTeam/data/refs/heads/main/notify"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SadoXTeam/data/refs/heads/main/main.lua"))()
